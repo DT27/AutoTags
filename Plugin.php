aa4c466abccadca8cf84458bd19f0055ed0398b2
DT27 <dt27@dt27.cn>
2016-04-01 21:26:34 +0800

    玻森 API 从关键词改为分词，优先匹配博客中现有的标签。

--- Plugin.php
+++ Plugin.php
@@ -19,7 +19,6 @@ class AutoTags_Plugin implements Typecho_Plugin_Interface
      */
     public static function activate()
     {
-        //Typecho_Plugin::factory('admin/menu.php')->navBar = array('AutoTags_Plugin', 'render');
         Typecho_Plugin::factory('Widget_Contents_Post_Edit')->write = array('AutoTags_Plugin', 'write');
 
     }
@@ -69,13 +68,23 @@ class AutoTags_Plugin implements Typecho_Plugin_Interface
      */
     public static function write($contents, $edit)
     {
+        $html = $contents['text'];
+        $isMarkdown = (0 === strpos($html, '<!--markdown-->'));
+        if($isMarkdown){
+            $html = Markdown::convert($html);
+        }
+        $text = str_replace("\n", '', trim(strip_tags(html_entity_decode($html))));
         $autoTags = Typecho_Widget::widget('Widget_Options')->plugin('AutoTags');
         //插件启用,且未手动设置标签
         if($autoTags->isActive == 1 && !$contents['tags']) {
+            Typecho_Widget::widget('Widget_Metas_Tag_Admin')->to($tags);
+            foreach($tags->stack as $tag){
+                $tagNames[] = $tag['name'];
+            }
             //str_replace("\n", '', trim(strip_tags($contents['text'])))
             //过滤 html 标签等无用内容
-            $postString = json_encode(str_replace("\n", '', trim(strip_tags($contents['text']))));
-            $ch = curl_init('http://api.bosonnlp.com/keywords/analysis?top_k=5');
+            $postString = json_encode($text);
+            $ch = curl_init('http://api.bosonnlp.com/tag/analysis?space_mode=0&oov_level=0&t2s=0');
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
             curl_setopt($ch, CURLOPT_POSTFIELDS,$postString);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
@@ -89,10 +98,37 @@ class AutoTags_Plugin implements Typecho_Plugin_Interface
             $result = curl_exec($ch);
             curl_close($ch);
             $result = json_decode($result);
-            foreach($result as $re){
-                $a[] = $re[1];
+            $ignoreTag = array('w', 'wkz', 'wky', 'wyz', 'wyy', 'wj', 'ww', 'wt', 'wd', 'wf', 'wn', 'wm', 'ws', 'wp', 'wb', 'wh', 'email', 'tel', 'id', 'ip', 'url', 'o', 'y', 'u', 'uzhe', 'ule', 'ugou', 'ude', 'usou', 'udeng', 'uyy', 'udh', 'uzhi', 'ulian', 'c', 'p', 'pba', 'pbei', 'd', 'dl', 'q', 'm', 'r', 'z', 'b', 'bl', 'a', 'ad', 'an', 'al', 'v', 'vd', 'vshi', 'vyou', 'vl', 'f', 's', 't', 'nl');
+            $sourceTags = array();
+            foreach($result[0]->tag as $key => $tag){
+                if(!in_array($tag, $ignoreTag)){
+                    if(in_array($result[0]->word[$key], $tagNames)){
+                        if(in_array($result[0]->word[$key], $sourceTags)) continue;
+                        $sourceTags[] = $result[0]->word[$key];
+                    }
+                }
             }
-            $contents['tags'] = implode(',', $a);
+            $contents['tags'] = implode(',', array_unique($sourceTags));
+            if(count($contents['tags'])<3){
+                $ch = curl_init('http://api.bosonnlp.com/keywords/analysis?top_k=5');
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_POSTFIELDS,$postString);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER,
+                    array(
+                        'Content-Type: application/json',
+                        'Accept: application/json',
+                        'X-Token: fpm1fDvA.5220.GimJs8QvViSK'
+                    )
+                );
+                $result = curl_exec($ch);
+                curl_close($ch);
+                $result = json_decode($result);
+                foreach($result as $re){
+                    $a[] = $re[1];
+                }
+                $contents['tags'] = $contents['tags']?$contents['tags'].','.implode(',', $a):implode(',', $a);
+            }
         }
         return $contents;
     }
